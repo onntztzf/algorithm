9c424e0af433f047e8127530257d1708d6dcd328
zhangpeng <zhangpeng.0304@aliyun.com>
2020-07-03 11:19:55 +0800

    35.search-insert-position.go

---
+++ 35.search-insert-position.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(searchInsert([]int{1,3,5,6}, 0))
+}
+
+func searchInsert(nums []int, target int) int {
+	idx:=0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target{
+			return i
+		}
+		if nums[i] > target{
+			return idx
+		} else {
+			idx++
+		}
+	}
+	return idx
+}
